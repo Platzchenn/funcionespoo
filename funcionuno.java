b3d8d3f969cfdf060a3118d39e71563e2d7c5ffe
Platzchenn <1034776102@teinco.edu.co>
2025-03-26 10:33:51 -0500

    Add files via upload

---
+++ funcionuno.java
@@ -0,0 +1,16 @@
+// Creación de la función sumar dos números en Java
+
+public class Ejemplo {
+    // Creación de la función
+
+    public static int sumar(int a, int b){
+        return a + b;
+    }
+
+    // Ejecución de la función sumar
+
+    public static void main(String[] args) {
+        int resultado = sumar(5, 3);
+        System.out.println(resultado); 
+    }
+}
